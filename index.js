3701e895b8c6f197569d1db58b981cf80baa4ad2
Mirna <mirna@pixelwaxshop.com>
2019-09-20 13:43:44 +0200

    Update path

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ sendmail({
     console.dir(reply);
 });
  */
+const fs = require('fs');
 let express = require("express"),
     path = require('path'),
     nodeMailer = require('nodemailer'),
@@ -41,7 +42,7 @@ app.post('/send-email', function(req, res) {
         attachments: [{
             // file on disk as an attachment
             filename: req.body.file,
-            path: req.body.file.filename // stream this file
+            path: fs.createReadStream(req.body.file.filename) // stream this file
         }]
     };
     transporter.sendMail(mailOptions, (error, info) => {
